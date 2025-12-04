7420bddd4aad7b39567d9077a8f03eb1170d6129
heavoice <muhammadwafiq1313@gmail.com>
2025-12-04 20:45:20 +0700

    update form pendaftaran

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -5,6 +5,7 @@ import Alumni from "./pages/Alumni.jsx";
 import News from "./pages/News.jsx";
 import NewsDetail from "./pages/NewsDetail.jsx";
 import Merch from "./pages/Merch.jsx";
+import FormHima from "./pages/FormHima.jsx";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ScrollToTop from "./components/ScrollToTop.jsx";
@@ -22,6 +23,7 @@ function App() {
           <Route path="/news/:id" element={ <NewsDetail/> } />
         <Route path="/data-alumni" element={ <Alumni/> } />
         <Route path="/merch" element={<Merch/>}/>
+        <Route path="/daftar-hima" element={ <FormHima/> } />
       </Routes>
     </main>
 
---
+++ frontEnd/src/data/FormData.jsx
@@ -0,0 +1,10 @@
+export const formFields = [
+  { id: "nama", label: "Nama Lengkap", placeholder: "Masukkan nama lengkap", type: "textarea" },
+  { id: "nim", label: "NIM", placeholder: "Masukkan NIM", type: "textarea" },
+  { id: "kelas", label: "Kelas", placeholder: "Masukkan kelas", type: "textarea" },
+  { id: "angkatan", label: "Angkatan", placeholder: "Masukkan angkatan", type: "textarea" },
+  { id: "alasan", label: "Alasan Masuk Hima", placeholder: "Masukkan alasan masuk Hima", type: "textarea", rows: 10 },
+  { id: "pengalaman", label: "Pengalaman Organisasi", placeholder: "Masukkan pengalaman organisasi", type: "textarea", rows: 10 },
+  { id: "cv", label: "Upload CV", placeholder: "", type: "file", rows: 10 },
+  { id: "ktm", label: "Upload KTM", placeholder: "", type: "file", rows: 10 },
+];
